5787e9e9dcbf5b8ceffdc11865225fe36bedb6eb
Hans Yadav <hansy@users.noreply.github.com>
2018-09-05 18:21:02 -0400

    remove path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/algolia-restaurant-assignment/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
