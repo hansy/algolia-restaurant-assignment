a749364d49a9787627a7d68116f227737923de16
Hans Yadav <hansy@users.noreply.github.com>
2018-09-05 17:41:40 -0400

    cleanup

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,7 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  ALGOLIA_KEY: '"13dc387d499b4b224443d0f2bfd6de91"',
+  ALGOLIA_INDEX: '"Restaurants"',
+  ALGOLIA_APPLICATION_ID: '"GXA0HF52CT"',
 }
